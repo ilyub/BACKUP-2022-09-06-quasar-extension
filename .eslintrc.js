5e7c383916881c856e5b2350c4f06b027fc2ba53
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2021-12-11 03:03:48 +0300

    chore(refactor,eslint)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,3 @@
 module.exports = {
-  extends: [require.resolve("@skylib/config/src/eslintrc")],
-  overrides: [
-    {
-      files: ["./src/App.vue", "./src/router/index.ts"],
-      rules: {
-        "import/no-extraneous-dependencies": [
-          "warn",
-          {
-            bundledDependencies: false,
-            devDependencies: true,
-            optionalDependencies: false,
-            peerDependencies: true
-          }
-        ],
-        "vue/no-bare-strings-in-template": "off"
-      }
-    }
-  ]
+  extends: [require.resolve("@skylib/config/src/eslintrc")]
 };
