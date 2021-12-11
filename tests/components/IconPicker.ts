779e3614abffbe49a44f74126c3554d2072ab226
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2021-12-11 20:04:59 +0300

    test(fix,IconPicker): Fix for failed test

--- tests/components/IconPicker.ts
+++ tests/components/IconPicker.ts
@@ -1,9 +1,19 @@
 import * as testUtils from "@vue/test-utils";
 
 import IconPicker from "@/components/IconPicker.vue";
+import Input from "@/components/Input.vue";
+import NavButton from "@/components/NavButton.vue";
 
 it("IconPicker", () => {
-  const wrapper = testUtils.mount(IconPicker);
+  const wrapper = testUtils.mount(IconPicker, {
+    components: {
+      [Input.name]: Input,
+      [NavButton.name]: NavButton
+    },
+    props: {
+      placeholder: ""
+    }
+  });
 
   expect(wrapper).toBeDefined();
 });
