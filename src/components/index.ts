da947c6b8afb1b3034ae3e76b6addb323f6e8c4f
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2022-02-07 00:35:19 +0300

    fix(BaseButton,LanguagePicker): Added .extras to index

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,11 +1,12 @@
 /* eslint-disable import/no-unassigned-import */
 
+import "./BaseButton.extras";
 import "./Card.extras";
 import "./DatetimePicker.extras";
 import "./Group.extras";
 import "./IconPicker.extras";
-import "./NumericInput.extras";
 import "./LanguagePicker.extras";
+import "./NumericInput.extras";
 import "./OptionGroup.extras";
 import "./PageLayout.extras";
 import "./PageTable.extras";
