9ce9a8deddb9147e7b3404e76581f0a26090cd61
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2022-02-09 10:35:09 +0300

    chore(eslint): Disabled "@skylib/require-jsdoc" and "vue/no-multiple-template-root" rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,15 @@
 module.exports = {
   extends: [require.resolve("@skylib/config/src/eslintrc")],
   rules: {
-    "vue/match-component-file-name": "off"
+    "vue/match-component-file-name": "off",
+    "vue/no-multiple-template-root": "off"
   },
   // @skylib/sort-keys break
   overrides: [
     {
       files: ["*.vue"],
       rules: {
-        "@skylib/require-jsdoc": "off",
-        "vue/no-multiple-template-root": "off"
+        "@skylib/require-jsdoc": "off"
       }
     }
   ]
