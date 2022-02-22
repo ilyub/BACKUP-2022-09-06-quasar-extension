f1502f15bd95cf5aef457b1b42dd6e794503acde
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2022-02-22 05:35:12 +0300

    chore(eslint): Edited configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,3 @@
 module.exports = {
-  extends: [require.resolve("@skylib/config/src/eslintrc")],
-  rules: {
-    "@typescript-eslint/no-empty-interface": "off",
-    "vue/match-component-file-name": "off",
-    "vue/no-multiple-template-root": "off",
-    "vue/no-unregistered-components": [
-      "warn",
-      { ignorePatterns: [/^[mq]-/u.source] }
-    ]
-  },
-  // @skylib/sort-keys break
-  overrides: [
-    {
-      files: ["*.vue"],
-      rules: {
-        "@skylib/require-jsdoc": "off"
-      }
-    }
-  ]
+  extends: [require.resolve("@skylib/config/src/eslintrc")]
 };
