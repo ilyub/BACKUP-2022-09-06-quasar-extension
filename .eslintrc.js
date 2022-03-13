059e9477975fb59aac149668c51d563f64c8309d
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2022-03-13 14:21:56 +0300

    chore(eslint): Edited rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,6 @@
 module.exports = {
   extends: [require.resolve("@skylib/config/src/eslintrc")],
   rules: {
-    // eslint-disable-next-line @skylib/disallow-by-regexp
-    // temp
-    "@skylib/no-mutable-signature": "off",
-    "@typescript-eslint/naming-convention": [
-      "warn",
-      {
-        format: ["camelCase"],
-        leadingUnderscore: "allow",
-        selector: ["default"]
-      },
-      {
-        format: ["camelCase", "PascalCase"],
-        leadingUnderscore: "allow",
-        selector: ["function", "typeLike", "variable"]
-      },
-      {
-        // eslint-disable-next-line unicorn/no-null
-        format: null,
-        selector: [
-          "objectLiteralMethod",
-          "objectLiteralProperty",
-          "typeProperty"
-        ]
-      }
-    ],
-    "pii/no-phone-number": "off",
-    "vue-scoped-css/enforce-style-type": ["warn", { allows: ["module"] }],
-    "vue/custom-event-name-casing": ["warn", "camelCase"],
-    "vue/v-on-function-call": ["warn", "never"]
+    "tsdoc/syntax": "off"
   }
 };
