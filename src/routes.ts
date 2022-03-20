8f006bd8863b3d3e8c4507f9bb088c6504987580
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2022-03-20 11:27:48 +0300

    chore(fix,routes): Fix for missing "/back" route

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,24 @@
 import type { RouteRecordRaw } from "vue-router";
 
 import { components } from "./samples";
+import Index from "./samples/Index.vue";
 
 export const routes: readonly RouteRecordRaw[] = [
+  {
+    component: Index,
+    path: "/"
+  },
   ...components.map(component => {
     return {
       component,
       name: component.name,
       path: `/${component.name}`
     };
-  })
+  }),
+  {
+    path: "/:catchAll(.*)",
+    redirect(): string {
+      return "/";
+    }
+  }
 ];
