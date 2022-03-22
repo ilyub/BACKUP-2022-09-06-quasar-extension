947b428a665425c79cb0ff4286ebf47e8c29185c
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2022-03-22 03:56:34 +0300

    test(fix,Table): Fix for failed test

--- tests/components/Table.ts
+++ tests/components/Table.ts
@@ -519,6 +519,7 @@ test.each([
       vueTestUtils.mount(Table, {
         global: testUtils.globalMountOptions(),
         props: o.removeUndefinedKeys<TableOwnProps>({
+          binaryStateSortOn: true,
           columns: [
             {
               align: "left",
