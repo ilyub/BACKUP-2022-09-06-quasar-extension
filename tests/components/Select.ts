3c684772c0dc1ec676976f4affa3f8e7c749698f
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2022-03-26 00:05:20 +0300

    test(Select): Better coverage

--- tests/components/Select.ts
+++ tests/components/Select.ts
@@ -9,6 +9,8 @@ test.each([
   { label: "Option 2", value: 1 },
   { label: "Option 3", value: "a" }
 ])("select", option => {
+  // eslint-disable-next-line no-warning-comments
+  // fixme: Use typedef
   const options: SelectOptions = [
     { disable: true, label: "Option 1", value: undefined },
     { label: "Option 2", value: 1 },
@@ -26,4 +28,25 @@ test.each([
 
   wrapper.findComponent(QSelect).vm.$emit("update:modelValue", option);
   expect(wrapper.emitted("update:modelValue")).toStrictEqual(expected);
+});
+
+test.each([
+  {
+    expected: "Select"
+  },
+  {
+    expected: "Custom label",
+    initialLabel: "Custom label"
+  }
+])("initial label", ({ expected, initialLabel }) => {
+  // eslint-disable-next-line no-warning-comments
+  // fixme: Use typedef
+  const options: SelectOptions = [
+    { label: "Option 1", value: 1 },
+    { label: "Option 2", value: "a" }
+  ];
+
+  const wrapper = testUtils.mount(Select, { props: { initialLabel, options } });
+
+  expect(wrapper.find(".q-field__native").text()).toStrictEqual(expected);
 });
