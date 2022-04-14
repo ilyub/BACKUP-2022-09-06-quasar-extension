9fbd3247430229411d925981453c5fd5ae8a63d8
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2022-04-14 09:23:43 +0300

    refactor

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,11 @@ module.exports = {
   // eslint-disable-next-line no-warning-comments
   // fixme
   rules: {
+    "@skylib/no-multi-type-tuples": "off",
     "@skylib/no-mutable-signature": "off",
     "@skylib/no-unsafe-object-assignment": "off",
     "@skylib/prefer-readonly": "off",
+    "@skylib/prefer-readonly-props": "off",
     "@typescript-eslint/no-empty-interface": "off",
     "eslint-comments/require-description": "off"
   }
