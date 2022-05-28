992fa3e7aa10fcccc64a0540134b63f0c5ab6644
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2022-05-28 18:19:44 +0300

    fix(Form): Fix for async submit button staying disabled

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -61,7 +61,7 @@ export default defineComponent({
 
                 try {
                   await props.onSubmitAsync(event);
-                } catch {
+                } finally {
                   disable.value--;
                 }
               }
