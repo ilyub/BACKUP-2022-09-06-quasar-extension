16ea82c93ac2c20bb3d955b0d6e04b02bc6b2161
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2022-06-12 00:07:53 +0300

    chore(refactor)

--- src/samples/Field.vue
+++ src/samples/Field.vue
@@ -1,27 +1,28 @@
 <script lang="ts">
 import { generic } from "..";
 import { Field } from "./Field.extras";
-import { as, is, o } from "@skylib/functions";
+import { as, cast, o } from "@skylib/functions";
 import { defineComponent, ref } from "vue";
 import type { extras } from "..";
+import type { stringU } from "@skylib/functions";
 
 export default defineComponent({
   name: "sample-field",
   components: {
     // eslint-disable-next-line vue/component-options-name-casing -- Wait for https://github.com/vuejs/eslint-plugin-vue/issues/1908
-    "m-field__string": generic.Field<string>()
+    "m-field__string": generic.Field<stringU>()
   },
   setup: () => {
     return {
       input: (
         event: Event,
-        emitValue: extras.Field.ControlSlotData<string>["emitValue"]
+        emitValue: extras.Field.ControlSlotData<stringU>["emitValue"]
       ): void => {
         // eslint-disable-next-line no-restricted-syntax -- Ok
-        emitValue(o.get(as.not.empty(event.target), "value", is.string));
+        emitValue(cast.stringU(o.get(as.not.empty(event.target), "value")));
       },
       lk: Field.lang.keys,
-      value: ref("")
+      value: ref()
     };
   }
 });
