163f930782789c833cfefbb4fd42c6ce6fefb66b
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2022-06-15 15:27:32 +0300

    test(fix): Better coverage

---
+++ tests/components/api/types/index.ts
@@ -0,0 +1,6 @@
+// eslint-disable-next-line @skylib/consistent-import, import/no-internal-modules -- Ok
+import * as types from "@/components/api/types";
+
+test("types", () => {
+  expect(types).toBeDefined();
+});
