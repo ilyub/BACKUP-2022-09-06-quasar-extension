6276cb663228ae411e14244ceedf7b2de3b3b3c0
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2022-06-15 16:24:32 +0300

    chore(commitlint): Edited scopes

--- commitlint-all.scopes.js
+++ commitlint-all.scopes.js
@@ -1,1 +1,33 @@
-module.exports = {};
+module.exports = {
+  scopes: [
+    "ListItem",
+    "NavButton",
+    "PageTable",
+    "PageMarkupTable",
+    "Tab",
+    "TabPanel",
+    "TabPanels",
+    "Tabs",
+    "XBaseButton",
+    "XCard",
+    "XIconButton",
+    "XInput",
+    "XNumericInput",
+    "XTooltip",
+    // eslint-disable-next-line no-warning-comments -- Postponed
+    // fixme - Do not use Russian "c"
+    "async-сlick",
+    "components.api",
+    "components.api.index",
+    "components.api.injection",
+    "components.api.pageContentHeight",
+    "components.api.rootElement",
+    "components.extras",
+    "components.index",
+    "confirmedClickModule",
+    "package-scripts",
+    "testUtils",
+    "typings",
+    "vueStorage"
+  ]
+};
