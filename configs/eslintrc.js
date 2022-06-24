28e657a38c98e4f5a861122fb878a7a09e6e3c37
Ilia Liubinskii <admin.phpdev@gmail.com>
2022-06-25 02:35:11 +0300

    fix(eslint,configs): Allow non-lang caption in q-item

--- configs/eslintrc.js
+++ configs/eslintrc.js
@@ -355,7 +355,7 @@ module.exports = {
       {
         message: "Use lang variable",
         selector:
-          "VAttribute > VIdentifier.key[name=/^(?:alt|aria-label|aria-placeholder|aria-roledescription|aria-valuetext|caption|confirmation|label|placeholder|title|tooltip|validation-label)$/u]"
+          "VElement[name=/^m-/u] > VStartTag > VAttribute > VIdentifier.key[name=/^(?:alt|aria-label|aria-placeholder|aria-roledescription|aria-valuetext|caption|confirmation|label|placeholder|title|tooltip|validation-label)$/u]"
       },
       {
         message: "Unexpected empty line",
