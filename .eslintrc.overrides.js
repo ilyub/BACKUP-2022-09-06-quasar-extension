85242344644d6d314c432aafc1f78c2142b877c0
Ilia Liubinskii <admin.phpdev@gmail.com>
2022-06-24 23:22:40 +0300

    chore(fix): Fix after "@skylib/config" update

--- .eslintrc.overrides.js
+++ .eslintrc.overrides.js
@@ -1,115 +1,15 @@
-const { eslint } = require("@skylib/config");
-
 module.exports = {
   overrides: [
     {
-      files: "./src/components/api/types/prop-options.ts",
-      // eslint-disable-next-line @skylib/no-restricted-syntax -- Postponed
-      rules: { "@skylib/primary-export-only": "off" }
+      extends: require.resolve("@skylib/config/src/eslintrc.chore"),
+      files: ["./src/App.vue", "./src/{application,router,samples}/**"]
     },
     {
-      files: [
-        "./src/components/*.generic.ts",
-        "./src/components/*.internal.ts",
-        "./src/components/*.extras.ts",
-        "./src/samples/*.generic.ts",
-        "./src/samples/*.internal.ts",
-        "./src/samples/*.extras.ts"
+      extends: [
+        require.resolve("@skylib/config/src/eslintrc.allow-default-export"),
+        require.resolve("@skylib/config/src/eslintrc.allow-global-access")
       ],
-      rules: {
-        // eslint-disable-next-line no-warning-comments -- Wait for @skylib/eslint-plugin update
-        // fixme
-        // eslint-disable-next-line @skylib/no-restricted-syntax -- Postponed
-        "@skylib/consistent-filename": "off",
-        // eslint-disable-next-line no-warning-comments -- Wait for @skylib/eslint-plugin update
-        // fixme
-        // eslint-disable-next-line @skylib/no-restricted-syntax -- Postponed
-        "@skylib/only-export-name": "off"
-      }
-    },
-    {
-      files: "./src/components/*.vue",
-      rules: { "@skylib/vue-component-name": ["warn", { prefix: "m-" }] }
-    },
-    {
-      files: "./src/facade-implementations/*/index.ts",
-      // eslint-disable-next-line @skylib/no-restricted-syntax -- Postponed
-      rules: { "@skylib/only-export-name": "off" }
-    },
-    {
-      files: "./src/samples/*.vue",
-      rules: { "@skylib/vue-component-name": ["warn", { prefix: "sample-" }] }
+      files: "./src/{boot,router}/*"
     }
-  ],
-  rules: {
-    "@skylib/optional-property-style": [
-      "warn",
-      {
-        classes: "undefined",
-        interfaces: "optional",
-        overrides: [
-          {
-            patterns: ["Props$"],
-            style: "combined",
-            target: "interfaces"
-          },
-          {
-            patterns: ["Props$"],
-            propertyPatterns: ["^on[A-Z]"],
-            style: "optional",
-            target: "interfaces"
-          }
-        ]
-      }
-    ],
-    "boundaries/element-types": [
-      "warn",
-      {
-        default: "disallow",
-        rules: [
-          ...eslint.boundaries.elementTypes.rules,
-          {
-            allow: "{src1,src2,src3,src4,src5,src6,src7}",
-            from: [
-              ["{src2,src3,src4,src5,src6,src7}", { dir1: "{boot,samples}" }]
-            ]
-          },
-          ...eslint.boundaries.elementTypes.createRules(
-            filename => ["src2", { dir1: "components", filename }],
-            ["Form", "Tooltip"],
-            ["BaseButton", "Field", "Item", "Menu", "Sortable", "Switchable"],
-            [
-              "Button",
-              "DatetimePicker",
-              "Droppable",
-              "FormActions",
-              "FormButton",
-              "IconButton",
-              "FormSection",
-              "Input",
-              "MenuItem",
-              "NumericInput",
-              "PageSection",
-              "Section",
-              "Subsection",
-              "Table"
-            ],
-            ["Group", "IconPicker", "LanguagePicker", "TimeInput"]
-          ),
-          ...eslint.boundaries.elementTypes.createRules(
-            filename => [
-              "src3",
-              {
-                dir1: "components",
-                dir2: "api",
-                filename
-              }
-            ],
-            "misc",
-            "*"
-          )
-        ]
-      }
-    ]
-  }
+  ]
 };
